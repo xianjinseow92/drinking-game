8246877ed5fb631677615c0378af16a61d2c60f4
Xian Jin <xianjinseow92@gmail.com>
2021-10-20 14:05:50 +0800

    Add temporary content for GameSelect Page

--- src/components/GameSelectPage.tsx
+++ src/components/GameSelectPage.tsx
@@ -4,6 +4,7 @@ import { useHistory } from "react-router";
 // Layout
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 // UI Components
 import Button from "@mui/material/Button";
@@ -11,6 +12,9 @@ import Button from "@mui/material/Button";
 // Constants / Helpers
 import { cleanKebabString, removeAllSlashes } from "helpers/helpers";
 import { gameNames } from "constants/constants";
+
+// Typography
+import { Typography } from "@mui/material";
 
 /**
  * Component serves as a Welcome Page & Control Panel to allow users to choose the game they want to play.
@@ -27,14 +31,14 @@ const GameSelectPage = (props: any) => {
 
   return (
     // Game Grid
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={6}>
-        <Box sx={{ bgcolor: "#cfe8fc", color: "black" }}>xs=12, md=6</Box>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Box sx={{ bgcolor: "#cfe8fc", color: "black" }}>xs=12, md=6</Box>
-      </Grid>
-      <Grid item xs={12} md={6}>
+    <Container component="section">
+      <Box sx={{width: "100%", display: "flex", flexDirection: "column" }}>
+        <Typography
+          variant="h3"
+          sx={{ textAlign: "center", marginBottom: "20px" }}
+        >
+          Welcome to ze best drinking game of yo LIFE!
+        </Typography>
         <Button
           variant="contained"
           color="primary"
@@ -42,8 +46,26 @@ const GameSelectPage = (props: any) => {
         >
           {removeAllSlashes(cleanKebabString(gameNames.higherOrLower))}
         </Button>
-      </Grid>
-    </Grid>
+      </Box>
+
+      {/* <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ bgcolor: "#cfe8fc", color: "black" }}>xs=12, md=6</Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ bgcolor: "#cfe8fc", color: "black" }}>xs=12, md=6</Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={goToHigherOrLowerGame}
+          >
+            {removeAllSlashes(cleanKebabString(gameNames.higherOrLower))}
+          </Button>
+        </Grid>
+      </Grid> */}
+    </Container>
   );
 };
 
